528e88b2cedccac4fbbdfd9bb61d3ff6dd0661a1
Vladimir <v.milashevsky@gmail.com>
2024-03-10 17:05:39 +0300

    feat: add swager module, fix api.yaml file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configDotenv } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from "fs/promises";
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configDotenv();
   const port = process.env.PORT || 5000;
+
+  const file  = await readFile('./doc/api.yaml', 'utf8')
+  const swaggerDocument = yaml.load(file);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => console.log(`Server start on ${port} port.`));
 }
